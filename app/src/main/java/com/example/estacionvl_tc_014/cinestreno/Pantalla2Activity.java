56ce2c7ee56080648edf2e77621f5881cf2dd6d5
Dario Chamoro <dario.chamorro.v@gmail.com>
2016-02-12 08:54:02 -0500

    Palette implementado en Pantalla 2

--- app/src/main/java/com/example/estacionvl_tc_014/cinestreno/Pantalla2Activity.java
+++ app/src/main/java/com/example/estacionvl_tc_014/cinestreno/Pantalla2Activity.java
@@ -1,18 +1,22 @@
 package com.example.estacionvl_tc_014.cinestreno;
 
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.estacionvl_tc_014.cinestreno.models.Pelicula;
 import com.example.estacionvl_tc_014.cinestreno.util.C;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
-public class Pantalla2Activity extends AppCompatActivity {
+public class Pantalla2Activity extends AppCompatActivity implements Callback {
 
     CollapsingToolbarLayout collapsing;
     Toolbar toolbar;
@@ -33,8 +37,26 @@ public class Pantalla2Activity extends AppCompatActivity {
         collapsing.setTitle(p.getNombre());
 
         Uri uri = Uri.parse(p.getUrlImg());
-        Picasso.with(this).load(uri).into(img);
+        Picasso.with(this).load(uri).into(img,this);
 
+
+    }
+
+    @Override
+    public void onSuccess() {
+
+        BitmapDrawable drawable = (BitmapDrawable) img.getDrawable();
+        Palette palette = Palette.from(drawable.getBitmap()).generate();
+
+        int primary = ContextCompat.getColor(this, R.color.colorPrimary);
+        int color = palette.getLightVibrantColor(primary);
+
+        collapsing.setContentScrimColor(color);
+
+    }
+
+    @Override
+    public void onError() {
 
     }
 }
